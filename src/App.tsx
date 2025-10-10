0b60839c6e9aa6470d31aaaa0bd815650a9d69e8
konradschantz <konradschantz@gmail.com>
2025-10-10 10:53:37 +0200

    chore: scaffold react ts app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Hello, Peak-ish world 👋</h1>
+      <p>Din nye Vite + React + TypeScript app er klar til at blive udforsket.</p>
+    </main>
+  )
+}
+
+export default App
