85bfa6d110b79a837acbe98ae9d0475568795da7
konrad <konradschantz@gmail.com>
2025-11-04 17:39:42 +0100

    commit ænderinger til nyt logo

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,6 +1,6 @@
 import type { CSSProperties } from 'react'
 
-const LOGO_URL = '/brand-logo.svg'
+const LOGO_URL = import.meta.env.BASE_URL + 'fokus-logo.svg'
 
 type BrandLogoElement = 'div' | 'span' | 'h1' | 'header'
 
